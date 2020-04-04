6f070aacbedbc94b6f53fce8b6cbd4f1dfc09890
刘楚舒 <liuchushu10@gmail.com>
2020-04-04 14:52:54 +0800

    The State Pattern

---
+++ TheStatePattern/src/chushu10/github/io/state/State.java
@@ -0,0 +1,8 @@
+package chushu10.github.io.state;
+
+public interface State {
+    public void insertQuarter();
+    public void ejectQuarter();
+    public void turnCrank();
+    public void dispense();
+}
