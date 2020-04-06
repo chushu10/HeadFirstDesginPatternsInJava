6eb6ed68c4615f0c72fcebbc02ceee440c5f7371
刘楚舒 <liuchushu10@gmail.com>
2020-04-06 17:04:38 +0800

    RMI example

---
+++ TheProxyPattern/src/chushu10/github/io/service/MyRemote.java
@@ -0,0 +1,8 @@
+package chushu10.github.io.service;
+
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+
+public interface MyRemote extends Remote {
+    public String sayHello() throws RemoteException;
+}
